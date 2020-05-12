e389bb8919121105712fac8e28a84f0e56eaa192
Tim Finnigan <tim.finnigan1@gmail.com>
2020-05-12 08:30:32 -0400

    post process and format data

--- server.js
+++ server.js
@@ -11,11 +11,29 @@ const options = {
   },
 };
 
+let obj = {}; // formatted data
+
 request(options, function (error, response) {
   if (error) throw new Error(error);
 
-  fs.writeFile("twitter.json", response.body, function (err) {
+  let data = JSON.parse(response.body);
+
+  // Just get the data you care about
+  for (let i = 0; i < data.length; i++) {
+    obj[i] = {};
+    obj[i].user = data[i].user.name;
+    obj[i].text = data[i].text;
+  }
+
+  console.log(obj);
+
+  fs.writeFile("formatted_data.json", JSON.stringify(obj), function (err) {
     if (err) return console.log(err);
     console.log("JSON File written");
   });
+
+  //   fs.writeFile("all_twitter_data.json", response.body, function (err) {
+  //     if (err) return console.log(err);
+  //     console.log("JSON File written");
+  //   });
 });
