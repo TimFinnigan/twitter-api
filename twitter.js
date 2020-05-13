d795b6059e9832d154e9bb58f9b797ea0b1c5848
Tim Finnigan <tim.finnigan1@gmail.com>
2020-05-13 13:07:26 -0400

    update script to get formatted data

--- twitter.js
+++ twitter.js
@@ -11,15 +11,34 @@ const client = new Twitter({
 
 const params = { screen_name: "nodejs", tweet_mode: "extended", count: 200 };
 
+let obj = {}; // formatted data
+
 client.get("statuses/home_timeline.json", params, function (
   error,
   tweets,
   response
 ) {
   if (!error) {
-    fs.writeFile("all_twitter.json", JSON.stringify(tweets), function (err) {
+    let data = JSON.parse(response.body);
+
+    // Just get the data you care about
+    for (let i = 0; i < data.length; i++) {
+      obj[i] = {};
+      obj[i].date = data[i].created_at;
+      obj[i].user = data[i].user.name;
+      obj[i].text = data[i].full_text;
+    }
+
+    console.log(obj);
+
+    fs.writeFile("formatted_data.json", JSON.stringify(obj), function (err) {
       if (err) return console.log(err);
       console.log("JSON File written");
     });
+
+    // fs.writeFile("all_data.json", JSON.stringify(tweets), function (err) {
+    //   if (err) return console.log(err);
+    //   console.log("JSON File written");
+    // });
   }
 });
